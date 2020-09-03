d3368812333d89c429187bae771870b42bf76008
zhongcg <1225465572@qq.com>
2020-09-03 15:51:12 +0800

    Initial commit

---
+++ src/views/demo/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <el-color-picker v-model="color3" show-alpha></el-color-picker>
+</template>
+
+<script>
+export default {
+  name: "index",
+  data() {
+    return {
+      color3: 'rgba(19, 206, 102, 0.8)'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
